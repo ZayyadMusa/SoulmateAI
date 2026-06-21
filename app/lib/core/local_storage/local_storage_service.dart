2d7702c2f86bb07ae73100e0f0f02308ad561d97
Zeeboi98 <Zayyadm65@gmail.com>
2026-06-21 20:50:00 +0100

    feat(storage): register remaining hive models and open boxes

--- app/lib/core/local_storage/local_storage_service.dart
+++ app/lib/core/local_storage/local_storage_service.dart
@@ -2,6 +2,9 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import '../../features/daily_reflection/domain/models/reflection.dart';
 import '../../features/memory_jar/domain/models/memory.dart';
+import '../../features/supportive_chat/domain/models/chat_message.dart';
+import '../../features/voice_mode/domain/models/voice_profile.dart';
+import '../../features/nocturnal_mode/domain/models/dream.dart';
 
 class LocalStorageService {
   static Future<void> init() async {
@@ -10,9 +13,15 @@ class LocalStorageService {
     // Register Adapters
     Hive.registerAdapter(ReflectionAdapter());
     Hive.registerAdapter(MemoryAdapter());
+    Hive.registerAdapter(ChatMessageAdapter());
+    Hive.registerAdapter(VoiceProfileAdapter());
+    Hive.registerAdapter(DreamAdapter());
     
     // Open Boxes
     await Hive.openBox<Reflection>('reflections');
     await Hive.openBox<Memory>('memories');
+    await Hive.openBox<ChatMessage>('chat_messages');
+    await Hive.openBox<VoiceProfile>('voice_profiles');
+    await Hive.openBox<Dream>('dreams');
   }
 }
